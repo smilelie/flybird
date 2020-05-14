fe3137b97c098aabd1291ee92494ad9d57f8c891
wuxiaoyu <fox-fly@163.com>
2020-05-14 18:59:38 +0800

    delete

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import FirstPage from '../components/FirstPage.vue'
 import SecondPage from '../components/SP.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/FirstPage',
-    component: FirstPage
-  },
+
   {
     path: '/SP',
     component: SecondPage
